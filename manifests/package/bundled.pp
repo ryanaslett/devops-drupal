8ca44aae3f37bea7fe71312490dab7688e677997
Dimitri Tischenko <timidri@gmail.com>
2013-09-10 13:18:37 +0200

    Revamped drupal::package::bundled
    - Now installing required php modules
    - untar now creates folder in $installroot
    - the class is responsible for creating $docroot

--- manifests/package/bundled.pp
+++ manifests/package/bundled.pp
@@ -1,17 +1,32 @@
 class drupal::package::bundled (
+  $installroot = $drupal::installroot,
   $docroot = $drupal::docroot,
   $version = $drupal::drupalversion,
 ) {
 
-  # TODO: This logic is flawed if a user wants to install to an existing directory.
+  $php_modules = $osfamily ? {
+    'RedHat' =>  ['gd', 'pdo', 'xml'],
+    default  =>  ['gd', 'mbstring', 'pdo', 'xml'],
+  }
+
+  php::module { $php_modules:
+    ensure => installed,
+    before => Exec['install drupal'],
+  }
+
   file { "/tmp/drupal-${version}.tar.gz":
     ensure => file,
     source => "puppet:///modules/drupal/drupal-${version}.tar.gz",
     before => Exec['install drupal'],
   }
   exec { 'install drupal':
-    command => "/bin/tar -xf /tmp/drupal-${version}.tar.gz -C ${docroot}",
-    creates => $docroot,
+    command => "/bin/tar -xf /tmp/drupal-${version}.tar.gz",
+    cwd     => $installroot,
+    creates => "${installroot}/drupal-${version}",
+    before  => File[$docroot],
   }
-
+  file { $docroot:
+    ensure => symlink,
+    target => "drupal-${version}",
+  }
 }
