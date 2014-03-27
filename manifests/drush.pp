f6524be81ec9246c62d6f623e2a972c62d4108ed
Dimitri Tischenko <timidri@gmail.com>
2014-03-27 18:32:32 +0100

    adding support for new drush versions

--- manifests/drush.pp
+++ manifests/drush.pp
@@ -1,13 +1,24 @@
 class drupal::drush (
-  $docroot = $drupal::docroot,
-  $version = $drupal::drushversion
+  $docroot    = $drupal::docroot,
+  $version    = $drupal::drushversion,
+  $installdir = '/usr/local/share',
 ) {
   # When drush7 gets packaged, drop this crap
+
+  # adding support for new drush release location
+
+  if versioncmp($version, '5.9.9') > 0 {
+    $download_url =  "https://github.com/drush-ops/drush/archive/${version}.tar.gz"
+    $unpackdir = "drush-$version"
+  } else {
+    $download_url = "http://ftp.drupal.org/files/projects/drush-${version}.tar.gz"
+    $unpackdir = 'drush'
+  }
   if $drupal::installtype == 'remote' {
     exec { 'install drush':
-      command => "/bin/tar -xf /tmp/drush-${version}.tar.gz -C /usr/local/share && rm /tmp/drush-${version}.tar.gz",
-      onlyif  => "/usr/bin/curl http://ftp.drupal.org/files/projects/drush-${version}.tar.gz -o /tmp/drush-${version}.tar.gz",
-      creates => '/usr/local/share/drush',
+      command => "/bin/tar -xf /tmp/drush-${version}.tar.gz -C ${installdir} && rm /tmp/drush-${version}.tar.gz",
+      onlyif  => "/usr/bin/wget ${download_url} -O /tmp/drush-${version}.tar.gz",
+      creates => "${installdir}/${unpackdir}",
     }
   } else {
     file { "/tmp/drush-${version}.tar.gz":
@@ -16,14 +27,14 @@ class drupal::drush (
       before => Exec['install drush'],
     }
     exec { 'install drush':
-      command => "/bin/tar -xf /tmp/drush-${version}.tar.gz -C /usr/local/share",
-      creates => '/usr/local/share/drush',
+      command => "/bin/tar -xf /tmp/drush-${version}.tar.gz -C ${installdir}",
+      creates => "${isntalldir}/${unpackdir}",
     }
   }
 
   file { '/usr/local/bin/drush':
     ensure  => symlink,
-    target  => '/usr/local/share/drush/drush',
+    target  => "${installdir}/${unpackdir}/drush",
     require => Exec['install drush'],
   }
 
