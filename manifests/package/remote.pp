906634d4f924a6af2d9af7338eb8a2ce77656d36
Ryan Aslett <github@mixologic.com>
2014-07-07 19:08:15 -0700

    updated tar file to strip out the first subdirectory

--- manifests/package/remote.pp
+++ manifests/package/remote.pp
@@ -3,11 +3,9 @@ class drupal::package::remote (
   $version = $drupal::drupalversion,
 ) {
 
-  # TODO: This logic is flawed if a user wants to install to an existing directory.
   exec { 'install drupal':
-    command => "/bin/tar -xf /tmp/drupal-${version}.tar.gz -C ${docroot} && rm /tmp/drupal-${version}.tar.gz",
+    command => "/bin/tar -xf /tmp/drupal-${version}.tar.gz --strip=1 -C ${docroot}",
     onlyif  => "/usr/bin/curl http://ftp.drupal.org/files/projects/drupal-${version}.tar.gz -o /tmp/drupal-${version}.tar.gz",
-    creates => $docroot,
   }
 
 }
